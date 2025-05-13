ce9afed2cb8110660b4360e4b9642ef7a20458c1
VladimirMahlin <vladmahlin@gmail.com>
2025-05-13 13:12:55 +0300

    Initial commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+require("config.lazy")
+local builtin = require('telescope.builtin')
+
+-- disable arrow keys in normal mode
+vim.api.nvim_set_keymap('n', '<Up>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Left>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Right>', '', { noremap = true, silent = true })
+
+-- show line numbers
+vim.wo.number = true
+
+-- telescope setup
+vim.keymap.set('n', 'ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', 'fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', 'fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', 'fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- spaces instead of tab (for F#)
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+ {
+  "nvim-treesitter/nvim-treesitter",
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "c_sharp", "fsharp" },
+
+  sync_install = false,
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+  }
+ }
+}
